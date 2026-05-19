6d7e86b94a985688882726e0cbf5932b5fece18a
nikhilthegreat1 <sjtfaridabad@gmail.com>
2026-05-19 13:11:59 +0530

    new

--- src/pages/ArticleDetail.jsx
+++ src/pages/ArticleDetail.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { ArticleSocialMetaHelmet } from '../components/SocialShareMeta';
 import VideoEmbed, { getArticleVideoUrl } from '../components/VideoEmbed';
 import { getArticleById } from '../lib/api';
 import { formatRelativeTime, getArticleTimestamp } from '../lib/time';
@@ -58,7 +57,6 @@ export default function ArticleDetail() {
 
   return (
     <>
-      <ArticleSocialMetaHelmet article={article} articleId={id} />
       <section className="page-grid" style={{ gridTemplateColumns: '1fr' }}>
         <article className="card">
           <img src={article.image_url || '/news-images/faridabad.svg'} alt={article.title} />
